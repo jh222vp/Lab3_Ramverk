3fdaf67dc35190b4a43eb69fb64c6ff22f2e34e0
jh222vp <jh222vp@student.lnu.se>
2015-01-27 14:08:18 +0000

    Fler filer

--- RegistreringsApplikation/test/models/user_test.rb
+++ RegistreringsApplikation/test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+  test "Should not save without name" do
+    t = User.new
+    t.password = "testPassword"
+    t.key = "testKey"
+    assert_not t.save
+  end
 end
